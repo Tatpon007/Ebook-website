75bd97d42782d81febf441ff7062d90cbbd63398
Tatpon007 <tatsaponto@gmail.com>
2024-03-20 02:58:11 +0700

    test react
    i just begin with react i test components

--- ebook_website/src/App.js
+++ ebook_website/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Title =() => <h1>login เข้าใช้งาน</h1>
+const Description =() => <p>user ที่ทำการ register เเล้ว</p>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title/>
+      <Description/>
     </div>
   );
 }
