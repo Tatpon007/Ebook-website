977f61e53b783a00197090c3fa9139dbc1142e36
ARPOONS <164197547+ARPOONS@users.noreply.github.com>
2024-03-24 17:52:34 +0700

    Update App.js

--- ebook_website/src/App.js
+++ ebook_website/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/Login';
 const Title =() => <h1>login เข้าใช้งาน</h1>
 const Description =() => <p>user ที่ทำการ register เเล้ว</p>
 function App() {
